4dcd0eec6f483d0fbb633c0d86be797b05817098
sygerry <sygerry@163.com>
2022-10-14 23:58:39 +0800

    第二次代码提交，测试代码

--- First.c
+++ First.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-	int a = 0;
-	printf("%d\n",a);
+	
+	/*---------*/
+	int x1,y1,x2,y2;
+	double dis = 0;
+	
+	scanf("%d %d %d %d",&x1, &y1, &x2, &y2);
+	
+	dis = sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+	
+	printf("%.2f\n",dis);
+	
 	return 0;
 } 
---
+++ First2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+
+	int a = 0;
+	printf("a = \n", a);
+	return 0;
+}
+
